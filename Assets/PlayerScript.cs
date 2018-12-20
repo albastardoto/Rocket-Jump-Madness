1c3fe6c7d6f14fad405ce32f85dafbdd1fbf9139
Albert Koppelmaa <albert.koppelmaa@hotmail.com>
2018-12-20 18:07:26 +0100

    first content commit

---
+++ Assets/PlayerScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+	private Rigidbody playerRigidBody;
+	public float movementForce;
+	public float maxMovementSpeed;
+	private float horizontalInput;
+	// Use this for initialization
+	void Start () {
+		playerRigidBody=GetComponent<Rigidbody>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		horizontalInput = Input.GetAxis("Horizontal") ;
+	}
+	void FixedUpdate(){
+		if(playerRigidBody.velocity.magnitude<=maxMovementSpeed){
+			    playerRigidBody.AddForce(new Vector3(horizontalInput,0,0)*movementForce);
+		}
+	}
+}
---
+++ Assets/RocketLauncherScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketLauncherScript : MonoBehaviour {
+	private Transform RocketSpawnPoint;
+	public float RocketSpeed=20f;
+	public GameObject RocketPrefab;
+	void Start(){
+		RocketSpawnPoint=transform.GetChild(0);
+	}
+	// Update is called once per frame
+	void Update () {	
+
+		Vector2 direction= Input.mousePosition-Camera.main.WorldToScreenPoint(transform.position);
+		float angle=Mathf.Atan2(direction.y,direction.x)*Mathf.Rad2Deg;
+		Quaternion rotation=Quaternion.AngleAxis(angle,Vector3.forward);
+		transform.rotation=rotation;
+		if (Input.GetButtonDown("Fire1")){
+            GameObject Rocket = Instantiate(RocketPrefab, 	RocketSpawnPoint.transform.position, transform.rotation);
+			Rocket.GetComponent<Rigidbody>().velocity=RocketSpeed*Rocket.GetComponent<Transform>().right;
+		}
+	}
+}
