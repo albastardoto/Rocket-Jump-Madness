374d255e67494d410e939d396cd4381295e90388
Albert Koppelmaa <albert.koppelmaa@hotmail.com>
2018-12-23 14:37:22 +0100

    added explosion particles

---
+++ Assets/RocketScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketScript : MonoBehaviour {
+	public GameObject ExplosionParticles;
+	void OnTriggerEnter(Collider col){
+		Debug.Log("BOOM");
+		 Instantiate(ExplosionParticles,this.transform.position,Quaternion.identity);
+		 Destroy(gameObject);
+	}
+}
